618f6f8508c2643695d8ee5dbd3449c4f9902f9e
Shestakov Evgeny <lester_g@inbox.ru>
2019-06-14 00:43:25 +0300

    "3.5. Стороны прямоугольника.[#146953]"

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -1,6 +1,19 @@
 package ru.job4j.condition;
 
+/**
+ * SqArea
+ * @author Evgeny Shestakov
+ *  * @since 14.06.2019
+ *  * @version 1.0
+ */
+
 public class SqArea {
+    /**
+     * Method square.
+     * @param p периметр
+     * @param k коэффициент
+     * @return площадь
+     */
     public int square(int p, int k) {
         int h = (p / 2) / (1 + k);
         int w = h * k;
