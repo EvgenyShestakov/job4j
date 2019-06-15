313c78116a3d96c341f43159c16fb152149716cf
Shestakov Evgeny <lester_g@inbox.ru>
2019-06-15 13:25:43 +0300

    4.1. Глупый бот.[#146920]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+/**
+ * @author Evgeny Shestakov (lester_g@inbox.ru)
+ * @version 1.0
+ * @since 15.06.2019
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) { // заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить.
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
