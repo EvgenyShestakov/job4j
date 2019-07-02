90d24a942b2b22a361044b7dff864256bbdd5abd
Shestakov Evgeny <lester_g@inbox.ru>
2019-07-02 07:56:49 +0300

    6.5. Создать программу для сортировки массива методом перестановки.[#146944]

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -1,24 +1,24 @@
 package ru.job4j.array;
-
-
+/**BubbleSort
+ * @author Evgeny Shestakov (lester_g@inbox.ru)
+ * @version 1.0
+ * @since 01.07.2019
+ */
 public class BubbleSort {
+    /**Bubble sorting
+     * @param  mas
+     * @return mas
+     */
     public int[] bubbleDouble(int[] mas) {
-        boolean isSorted = false;
-        int buf;
-        while(!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < mas.length-1; i++) {
-                if(mas[i] > mas[i+1]){
-                    isSorted = false;
-
-                    buf = mas[i];
-                    mas[i] = mas[i+1];
-                    mas[i+1] = buf;
-
+        for (int i = 0; i < mas.length - 1; i++) {
+            for (int j = 0; j < mas.length - i - 1; j++) {
+                if (mas[j] > mas[j + 1]) {
+                    int tmp = mas[j];
+                    mas[j] = mas[j + 1];
+                    mas[j + 1] = tmp;
                 }
             }
         } return mas;
     }
 
 }
-
--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -10,8 +10,7 @@ public class BubbleSortTest {
         BubbleSort bubble = new BubbleSort();
         int [] mas = new int[] {1, 5, 4, 2, 3, 1, 7, 8, 0, 5};
         int [] result = bubble.bubbleDouble(mas);
-        int [] expect = new int[]{0, 1, 1, 2, 3, 4, 5, 5, 7, 8};
-        assertThat(result, is(expect));
+        assertThat(result, is(new int []{0, 1, 1, 2, 3, 4, 5, 5, 7, 8}));
         //напишите здесь тест, проверяющий сортировку массива из 10 элементов методом пузырька, например {1, 5, 4, 2, 3, 1, 7, 8, 0, 5}.
     }
 }
