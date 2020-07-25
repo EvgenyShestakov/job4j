89f39ed551f23334fecabce22d21ab1552a27cab
Shestakov Evgeny <lester_g@inbox.ru>
2020-07-25 13:37:45 +0300

    1.2.3.2 Реализовать класс Tracker [#147045]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,45 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item findById(int id) {
+        Item rsl = null;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getId() == id) {
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, this.size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] namesWithoutNull = new Item[this.size];
+        int count = 0;
+        for (int index = 0; index < this.size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                namesWithoutNull[count++] = item;
+            }
+        }
+        return Arrays.copyOf(namesWithoutNull, count);
+    }
+
+
 }
